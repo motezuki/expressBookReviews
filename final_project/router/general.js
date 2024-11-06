7381174c5b4717e030f6afab0acf7268e254d4c9
Fabio Motezuki <fabio.kenji@gmail.com>
2024-11-06 20:59:50 +0000

    Task 3 - Added code for getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  var list = [];
+  for (key in books){
+    if (books[key].author === author){
+      list.push(books[key]);
+    }
+  }
+  return res.send(JSON.stringify(list, null, 4));
 });
 
 // Get all books based on title
