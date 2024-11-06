d30f1073c322a045f2bba8f032fec8c248150cbf
Fabio Motezuki <fabio.kenji@gmail.com>
2024-11-06 23:42:13 +0000

    Task 7 - Added code to process the user login - Autheticate a user against the database - Encode username in token

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -16,13 +16,34 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  validUser = users.filter(user => user.username === username && user.password === password);
+  if (validUser.length > 0){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password)
+    return res.status(404).json({ message: "Error loggin in"});
+
+  if (authenticatedUser(username,password)){
+    let token = jwt.sign({data: username}, "access", {expiresIn: 60*60});
+
+    req.session.authorization = {
+      token, username
+    }
+    return res.status(200).json({message: "User logged in successfully"});
+  }
+  else {
+    return res.status(208).json({message: "Invalid login, please try again"});
+  }
 });
 
 // Add a book review
