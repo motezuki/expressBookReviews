30070ad6710c18bd9069f5b0c05ef795524ad447
Fabio Motezuki <fabio.kenji@gmail.com>
2024-11-06 23:13:02 +0000

    Task 5 - Added code to register new users - Verify if username is not already in use

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,19 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password){
+    return res.status(403).json({message: "Error logging in"});
+  }
+
+  if (isValid(username)){
+    users.push({"username":username, "password":password});
+    return res.status(200).json({message: "Successfully registered"});
+  }
+  else {
+    return res.status(403).json({message: "User " + username + " already exists"});
+  }
 });
 
 // Get the book list available in the shop
